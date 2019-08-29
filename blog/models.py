7ddae6b734cc662525bc91898cf5afbe0d487107
Ruoyu Xu <ryxusag@gmail.com>
2019-08-28 23:09:33 -0700

    Blog functioniality and polish

--- blog/models.py
+++ blog/models.py
@@ -12,10 +12,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-# Add the Blog app to the settings
+    def __str__(self):
+        return self.title
 
-# Create a migration
+    def summary(self):
+        return self.body[:100]
 
-# Migration
-
-# Add the admin
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
